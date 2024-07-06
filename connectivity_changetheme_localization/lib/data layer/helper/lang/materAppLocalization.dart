0759adda911660dae352d358a82e584bf8d9438d
fady emil <fady46t45f@gmail.com>
2024-07-06 18:52:24 +0300

    refactory part 1

--- connectivity_changetheme_localization/lib/data layer/helper/lang/materAppLocalization.dart
+++ connectivity_changetheme_localization/lib/data layer/helper/lang/materAppLocalization.dart
@@ -4,45 +4,48 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-class materApp extends StatelessWidget {
-  const materApp({
+class material_app extends StatelessWidget {
+  const material_app({
     super.key,
-    required this.locae,
+    required this.lang,
     required this.theme,
-    required this.messageinternet,
+    required this.message,
   });
 
-  final String? locae;
+  final String? lang;
   final String? theme;
-  final String? messageinternet;
+  final String? message;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      locale: Locale(locae!),
+      locale: Locale(lang!),
       //~ دعم اللغات المتوفرة
       supportedLocales: const [
         Locale('en'), // الإنجليزية
         Locale('ar'), // العربية
       ],
-
+    
       //~ القوائم المفوضة لترجمة النصوص وعناصر الواجهة
       localizationsDelegates: [
-        AppLocalizations.delegate, //^ المفوض الخاص بالترجمات المخصصة لتطبيقك
+        AppLocalizations
+            .delegate, //^ المفوض الخاص بالترجمات المخصصة لتطبيقك
         GlobalMaterialLocalizations
             .delegate, //^ المفوض لترجمات واجهات مكتبة الـMaterial
-        GlobalWidgetsLocalizations.delegate, //^ المفوض لترجمات الواجهات العامة
+        GlobalWidgetsLocalizations
+            .delegate, //^ المفوض لترجمات الواجهات العامة
         GlobalCupertinoLocalizations
             .delegate, //^ المفوض لترجمات واجهات مكتبة الـCupertino
       ],
-
+    
       //~ دالة لتحديد اللغة المفضلة بناءً على إعدادات جهاز المستخدم
-      localeListResolutionCallback: (deviceLocale, supportedLocales) {
+      localeListResolutionCallback:
+          (deviceLocale, supportedLocales) {
         //~ التحقق من اللغات المدعومة ومطابقتها مع لغة الجهاز
         for (var locale in supportedLocales) {
           if (deviceLocale != null) {
-            Locale device =
-                deviceLocale.first; //* الحصول على أول لغة مفضلة في الجهاز
+            Locale device = deviceLocale
+                .first; //* الحصول على أول لغة مفضلة في الجهاز
             if (device.languageCode == locale.languageCode) {
               return device; //* إعادة لغة الجهاز إذا كانت مدعومة
             }
@@ -51,11 +54,11 @@ class materApp extends StatelessWidget {
         return supportedLocales
             .first; //* إذا لم تكن لغة الجهاز مدعومة، يتم استخدام اللغة الافتراضية
       },
-
+    
       title: 'Flutter Demo',
       theme: theme == 'l' ? ThemeData.light() : ThemeData.dark(),
       home: MyHomePage(
-        message: messageinternet!,
+        message: message!,
       ),
     );
   }
--- connectivity_changetheme_localization/lib/main.dart
+++ connectivity_changetheme_localization/lib/main.dart
@@ -3,12 +3,13 @@ import 'package:connectivity_changetheme_localization/Business%20logic%20layer/c
 import 'package:connectivity_changetheme_localization/Business%20logic%20layer/language/language_bloc.dart';
 import 'package:connectivity_changetheme_localization/Business%20logic%20layer/theme/theme_bloc.dart';
 import 'package:connectivity_changetheme_localization/data%20layer/helper/contanst.dart';
-import 'package:connectivity_changetheme_localization/data%20layer/helper/lang/appLocalizations.dart';
+// import 'package:connectivity_changetheme_localization/data%20layer/helper/lang/appLocalizations.dart';
+import 'package:connectivity_changetheme_localization/data%20layer/helper/lang/materAppLocalization.dart';
 // import 'package:connectivity_changetheme_localization/data%20layer/helper/lang/materAppLocalization.dart';
-import 'package:connectivity_changetheme_localization/presentation%20layer/screen/MyHomePage.dart';
+// import 'package:connectivity_changetheme_localization/presentation%20layer/screen/MyHomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -46,49 +47,7 @@ class MyApp extends StatelessWidget {
                 var message = internetState is IntertetConnected
                     ? internetState.internetMessage
                     : 'internet Not connected';
-                return MaterialApp(
-                  locale: Locale(lang!),
-                  //~ دعم اللغات المتوفرة
-                  supportedLocales: const [
-                    Locale('en'), // الإنجليزية
-                    Locale('ar'), // العربية
-                  ],
-
-                  //~ القوائم المفوضة لترجمة النصوص وعناصر الواجهة
-                  localizationsDelegates: [
-                    AppLocalizations
-                        .delegate, //^ المفوض الخاص بالترجمات المخصصة لتطبيقك
-                    GlobalMaterialLocalizations
-                        .delegate, //^ المفوض لترجمات واجهات مكتبة الـMaterial
-                    GlobalWidgetsLocalizations
-                        .delegate, //^ المفوض لترجمات الواجهات العامة
-                    GlobalCupertinoLocalizations
-                        .delegate, //^ المفوض لترجمات واجهات مكتبة الـCupertino
-                  ],
-
-                  //~ دالة لتحديد اللغة المفضلة بناءً على إعدادات جهاز المستخدم
-                  localeListResolutionCallback:
-                      (deviceLocale, supportedLocales) {
-                    //~ التحقق من اللغات المدعومة ومطابقتها مع لغة الجهاز
-                    for (var locale in supportedLocales) {
-                      if (deviceLocale != null) {
-                        Locale device = deviceLocale
-                            .first; //* الحصول على أول لغة مفضلة في الجهاز
-                        if (device.languageCode == locale.languageCode) {
-                          return device; //* إعادة لغة الجهاز إذا كانت مدعومة
-                        }
-                      }
-                    }
-                    return supportedLocales
-                        .first; //* إذا لم تكن لغة الجهاز مدعومة، يتم استخدام اللغة الافتراضية
-                  },
-
-                  title: 'Flutter Demo',
-                  theme: theme == 'l' ? ThemeData.light() : ThemeData.dark(),
-                  home: MyHomePage(
-                    message: message!,
-                  ),
-                );
+                return material_app(lang: lang, theme: theme, message: message);
               },
             );
           },
@@ -97,3 +56,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
